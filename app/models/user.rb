ca9cbcf6ba9c49b362bec20c46146dc030df9316
kouichi <guangyihouteng8@gmail.com>
2019-05-01 08:54:28 +0000

    課題提出

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   validates :name, presence: true,  length: { maximum: 15 } 
     
-  PW_REGEX = /[A-Za-z0-9._+]+@[A-Za-z]+.[A-Za-z]/
-  validates :email, presence: true, format: { with: PW_REGEX }
+  EM_REGEX = /[A-Za-z0-9._+]+@[A-Za-z]+.[A-Za-z]/
+  validates :email, presence: true, format: { with: EM_REGEX }
     
   PW_REGEX = /\A(?=.＊?[a-z])(?=.＊?\d)[a-z\d]{8,32}\z/i
-validates :password, presence: true, format: { with: PW_REGEX }
+  validates :password, presence: true, format: { with: PW_REGEX }
   
   has_secure_password
   
