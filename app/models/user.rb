713b805a6ace30498f60f083b6b4b86b249cad82
kouichi <guangyihouteng8@gmail.com>
2019-05-01 08:23:19 +0000

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  validates :name, presence: true,  length: { maximum: 15 } 
+    
+  PW_REGEX = /[A-Za-z0-9._+]+@[A-Za-z]+.[A-Za-z]/
+  validates :email, presence: true, format: { with: PW_REGEX }
+    
+  PW_REGEX = /\A(?=.＊?[a-z])(?=.＊?\d)[a-z\d]{8,32}\z/i
+validates :password, presence: true, format: { with: PW_REGEX }
+  
+  has_secure_password
+  
+end
